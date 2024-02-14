1e888b5065a110b93b42e5e60b5599168602c538
Felipe Duarte Silva <feliped.dev@proton.me>
2024-02-14 06:33:04 +0000

    Add Vercel Analytics component to main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
 function Home() {
-    return <h1>Duda, te amo beleza?!</h1>
+  return (
+    <>
+      <h1>Duda, te amo beleza?!</h1>
+      <Analytics />
+    </>
+  );
 }
 
 export default Home;
